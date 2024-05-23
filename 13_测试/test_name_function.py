7d1a270010b700aa6ce9b129ec3adb893d73ea5a
chen <18551681083@163.com>
2024-05-23 22:18:02 +0800

    optimize comment

--- 13_测试/test_name_function.py
+++ 13_测试/test_name_function.py
@@ -1,21 +1,17 @@
 """
-执行单元测
-需要引入 unittest 模块
-测试类需要继承unittest.TestCase
-测试方法必须已test_开头
-如果当前模块作为main执行，则特殊变量__name__的值为__main__
-然后执行unittest.main()执行单元测试
+执行单元测需要引入 unittest 模块
 """
 
 import unittest
+
 from name_function import get_formatted_name
 
 
 class NamesTestCase(unittest.TestCase):
-    """测试name_function.py"""
+    """测试类需要继承 unittest.TestCase """
 
     def test_first_last_name(self):
-        """能够正确处理像Janis Joplin这样的姓名吗?"""
+        """测试方法必须已 test_ 开头"""
         formatted_name: str = get_formatted_name("janis", "joplin")
         self.assertEqual(formatted_name, "Janis Joplin")
 
@@ -25,5 +21,7 @@ class NamesTestCase(unittest.TestCase):
         self.assertEqual(formatted_name, "Wolfgang Amadeus Mozart")
 
 
+# 如果当前模块作为main执行，则特殊变量__name__的值为__main__
+# 然后执行unittest.main()执行单元测试
 if __name__ == '__main__':
     unittest.main()
