4ae058e22a6eb0837110a1c24c09df1baaab321d
chen <18551681083@163.com>
2024-05-24 23:02:40 +0800

    create ship class and render it on screen

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -2,6 +2,7 @@ import sys
 import pygame
 
 from settings import Settings
+from ship import Ship
 
 
 class AlienInvasion:
@@ -13,6 +14,7 @@ class AlienInvasion:
         pygame.display.set_caption("Alien Invasion")
         self.settings = Settings()
         self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
+        self.ship = Ship(self)
 
     def run_game(self):
         """开始游戏的主循环"""
@@ -23,6 +25,7 @@ class AlienInvasion:
                     sys.exit()
             # 每次循环时都重绘屏幕
             self.screen.fill(self.settings.bg_color)
+            self.ship.blit_me()
             # 让最近绘制的屏幕可见
             pygame.display.flip()
 
