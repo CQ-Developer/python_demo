ba6b1eb4d42b381694148cc661bb289d5c5245a2
NoahChen <noah.chen@batechworks.com>
2024-06-21 17:49:54 +0800

    n397. Integer Replacement alg. bit manipulation

---
+++ medium/397_Integer_Replacement.py
@@ -0,0 +1,20 @@
+class Solution:
+    def integerReplacement(self, n: int) -> int:
+        ans: int = 0
+        while n > 1:
+            if n & 1:
+                if n != 3 and n & 2 == 2:
+                    n += 1
+                else:
+                    n -= 1
+            else:
+                n >>= 1
+            ans += 1
+        return ans
+
+
+if __name__ == '__main__':
+    solution = Solution()
+    assert 2 == solution.integerReplacement(4)
+    assert 4 == solution.integerReplacement(7)
+    assert 3 == solution.integerReplacement(8)
